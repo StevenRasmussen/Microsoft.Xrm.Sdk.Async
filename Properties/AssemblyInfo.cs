5071d45a3f2bf2b0f510bcd51f460795ef4ed547
Steven Rasmussen <srasmussen@restaurant365.com>
2020-06-17 14:43:47 -0600

    Added nuspec and v9.0.5 nupkg

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Microsoft.Xrm.Sdk.Async")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Async version of Microsft.Xrm.Sdk")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Simpler Software")]
 [assembly: AssemblyProduct("Microsoft.Xrm.Sdk.Async")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -15,16 +15,21 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2ad885d6-2eb9-483e-bf6e-e2f860964a88")]
 
 #if SDK6
-[assembly: AssemblyVersion("6.0.4.0")]
-[assembly: AssemblyFileVersion("6.0.4.0")]
+[assembly: AssemblyVersion("6.0.5.0")]
+[assembly: AssemblyFileVersion("6.0.5.0")]
 #endif
 
 #if SDK7
-[assembly: AssemblyVersion("7.0.4.0")]
-[assembly: AssemblyFileVersion("7.0.4.0")]
+[assembly: AssemblyVersion("7.0.5.0")]
+[assembly: AssemblyFileVersion("7.0.5.0")]
 #endif
 
 #if SDK8
-[assembly: AssemblyVersion("8.0.4.0")]
-[assembly: AssemblyFileVersion("8.0.4.0")]
+[assembly: AssemblyVersion("8.0.5.0")]
+[assembly: AssemblyFileVersion("8.0.5.0")]
+#endif
+
+#if SDK9
+[assembly: AssemblyVersion("9.0.5.0")]
+[assembly: AssemblyFileVersion("9.0.5.0")]
 #endif
